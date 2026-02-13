e9736adc4dd68da5f5198f0110e7d1b696d810b9
Nicosmoss <nicolasdijoud@ik.me>
2026-02-13 12:58:41 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+const button = document.getElementById("validation");
+const input = document.getElementById("text");
+const card = document.getElementById("card");
+const manchot = document.getElementById("manchot");
+const loveYouToo = document.getElementById("loveYouToo");
+const sad = document.getElementById("sad");
+
+const answers = ["nico", "nicolas", "nicosmos", "nicolas dijoud", "dijoud", "nicolasdijoud", "nicodijoud"];
+button.addEventListener('click', () => {
+    let value = input.value.trim().toLowerCase();
+    console.log(value);
+    if (answers.includes(value)) {
+        processLove();
+    }
+    else {
+        processSadness();
+    }
+    input.value = "";
+
+});
+
+function processLove() {
+    card.style.display="none";
+    let b = document.getElementById("ballon");
+    b.classList.add("ballonVisible");
+    manchot.style.display = "block";
+    setTimeout( ()=>{
+        loveYouToo.style.display = "block";
+        manchot.style.display = "none";
+    }, 4000);
+
+}
+
+function processSadness() {
+    card.style.display="none";
+    sad.style.display = "block";
+    setTimeout( ()=>{
+        card.style.display = "flex";
+        sad.style.display = "none";
+    }, 5000)
+
+}
